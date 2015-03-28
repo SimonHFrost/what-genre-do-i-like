27de637c85dda1c1f87bb4cf5e5f9d8a8f737427
simonhfrost <simonhfrost@gmail.com>
2015-03-28 11:08:05 +0100

    get react list working

--- myReact.js
+++ myReact.js
@@ -1,13 +1,36 @@
-var MyComponent = React.createClass({
-    render: function(){
-        return (
-            <h1>Hello, world!</h1>
-        );
-    }
+/** @jsx React.DOM */
+var TodoList = React.createClass({
+  render: function() {
+    var createItem = function(itemText) {
+      return <li>{itemText}</li>;
+    };
+    return <ul>{this.props.items.map(createItem)}</ul>;
+  }
 });
 
-/** @jsx React.DOM */
-React.render(
-  <MyComponent/>,
-  document.getElementById('mount-point')
-);
+var TodoApp = React.createClass({
+  getInitialState: function() {
+    return {items: [], text: ''};
+  },
+  onChange: function(e) {
+    this.setState({text: e.target.value});
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var nextItems = this.state.items.concat([this.state.text]);
+    var nextText = '';
+    this.setState({items: nextItems, text: nextText});
+  },
+  render: function() {
+    return (
+      <div>
+        <TodoList items={this.state.items} />
+        <form className="form-inline" onSubmit={this.handleSubmit}>
+          <input className="form-control col-md-4" onChange={this.onChange} value={this.state.text} />
+        </form>
+      </div>
+    );
+  }
+});
+
+React.render(<TodoApp />, document.getElementById("content"));
